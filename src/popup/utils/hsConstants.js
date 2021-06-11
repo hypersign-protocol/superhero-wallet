f4f61f76b15f6c90cf598cfdc7739b105a065fd8
vishwas anand <vishwasanand.cse@gmail.com>
2021-06-11 18:46:19 +0530

    update stage urls

--- src/popup/utils/hsConstants.js
+++ src/popup/utils/hsConstants.js
@@ -1,5 +1,5 @@
-export const HS_NODE_BASE_URL = "https://ssi.hypermine.in/core/";
-export const SUPERHERO_HS_AUTH_BASE_URL = "https://ssi.hypermine.in/hsauth/"; // change supero to hypersign later
+export const HS_NODE_BASE_URL = "http://13.126.43.31/core/";
+export const SUPERHERO_HS_AUTH_BASE_URL = "http://13.126.43.31/hsauth/"; // change supero to hypersign later
 export const SUPERHERO_HS_AUTH_CREDENTIAL_ISSUE_API = "hs/api/v2/register"; // change supero to hypersign later
 export const HS_AUTH_DID_URL = SUPERHERO_HS_AUTH_BASE_URL + "hs/api/v2/authdid"
                                           
