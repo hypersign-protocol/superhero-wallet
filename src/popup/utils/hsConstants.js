0a94b7e71829406b7a4c5d46cf83e9afcf3612bd
vishwas anand <vishwasanand.cse@gmail.com>
2021-07-01 17:46:54 +0530

    inital commit

--- src/popup/utils/hsConstants.js
+++ src/popup/utils/hsConstants.js
@@ -1,5 +1,5 @@
-export const HS_NODE_BASE_URL = "https://stage.hypermine.in/core/";
-export const SUPERHERO_HS_AUTH_BASE_URL = "https://stage.hypermine.in/hsauth/"; // change supero to hypersign later
+export const HS_NODE_BASE_URL = "https://ssi.hypermine.in/core/";
+export const SUPERHERO_HS_AUTH_BASE_URL = "https://ssi.hypermine.in/hsauth/"; // change supero to hypersign later
 export const SUPERHERO_HS_AUTH_CREDENTIAL_ISSUE_API = "hs/api/v2/register"; // change supero to hypersign later
 export const HS_AUTH_DID_URL = SUPERHERO_HS_AUTH_BASE_URL + "hs/api/v2/authdid"
                                           
